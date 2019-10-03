7a3e1156df550203bd5e5e107bdadc1402945e69
DahaWong <dahawong@gmail.com>
2019-10-04 01:53:22 +0800

    Site updated: 2019-10-04 01:53:22

--- js/index.js
+++ js/index.js
@@ -1,8 +1,38 @@
 $(function() {
   $(".page-number").each(function(){
-    console.log($(this).text())
     if ($(this).text()=="1F"){
         $(this).addClass("active-page")
     }
   })
+
+  $("button").each(function(){
+    $(this).mousedown(function(){
+        let up = $(this).filter(".upstair")[0]
+        let down = $(this).filter(".downstair")[0]
+        let img = $(this).filter("img")[0]
+        img.each(function(){
+            $(this).attr("src","/img/active-triangle.png");
+            $(this).addClass("active-button")
+            console.log($('footer').offset().top - $(window).height())
+            console.log($(window).scrollTop())
+            $(window).scrollTop($('footer').offset().top - $(window).height())
+        })
+    })
+
+    $(this).mouseup(function(){
+        let img = $(this).find("img")
+        img.each(function(){
+            $(this).removeClass("active-button")
+            $(this).attr("src","/img/triangle.png")
+        })
+    })
+
+
+    // $(this).mouseup(function(){
+    //     console.log("up")
+    //     let img = $(this).find("img")
+    //     img.each(function(){
+    //         $(this).attr("src","/img/triangle.png")
+    //     })
+    })
 })
