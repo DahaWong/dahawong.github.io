ba42df578a0a681077941486b1582a8b736e69d2
DahaWong <dahawong@gmail.com>
2019-10-04 23:09:45 +0800

    Site updated: 2019-10-04 23:09:45

--- js/index.js
+++ js/index.js
@@ -1,4 +1,20 @@
 $(function() {
+    let currentTime = new Date();
+    let currentHour = currentTime.getHours();
+    // console.log(currentHour)
+    if ((currentHour >= 6) && (currentHour <= 19)){
+    }else{
+        $("*").css("transition","1")
+        $("body, .main-container").css("background-color","#303030")
+        $(".article-title").css({"background-color":"#212121", "margin-bottom":"1rem"})
+        $(".active-page").css("text-shadow","0 0 0.4rem white, 0 0 0.4rem #f5f5f5")
+        $(".paginator-container").css("background-color","#151515")
+        $("h1, h3, h4").css("color","#e5e5e5")
+        $("p, li, a, .from-to").css("color","#d4d4d4")
+        $("blockquote").css("border-left","0.07rem solid #BDBDBD")
+        $(".toc-container").css("color","#bdbdbd")
+    }
+
   $(".page-number").each(function(){
     if ($(this).text()=="1F"){
         $(this).addClass("active-page")
@@ -8,7 +24,6 @@ $(function() {
 
   $("button").each(function(){
     let up = $($(this).filter(".upstair")[0])
-    let down = $($(this).filter(".downstair")[0])
     let img = $($(this).find("img")[0])
 
     function isUp(button){
